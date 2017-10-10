5961e25bd8472e762388feb557bcc49127561088
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-10 13:35:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,39 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+def won?(board)
+  WIN_COMBINATIONS.each do |win|
+    if win.all?{|i| board[i] == "X"}
+      return win
+    elsif win.all?{|i| board[i] == "O"}
+      return win
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.none? do |token|
+    if token == " "
+      true
+    else
+      false
+    end
+  end
+end
+
+def draw?()
+  
+end
+
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS =[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [0,4,8],
+]
